658fcef3e9ef457d5b4d007593a166ae0d4c8ddd
mikhail.timofeev <mikhail.timofeev@lgepartner.com>
2023-07-18 17:49:11 +0300

    fix a littel

--- 11_Ansible/lemp_stack/ansible/roles/php/files/test-php/php_test/test-connection.php
+++ 11_Ansible/lemp_stack/ansible/roles/php/files/test-php/php_test/test-connection.php
@@ -1,25 +1,24 @@
 <?php
-$servername = "localhost"; //домен к которому мы подсоединяемся, поскольку все происходит на одной машине - это localhost
-$username = "root"; //имя пользователя которое мы ставили на шаге установки mysql
-$password = "password"; //пароль пользователя
+$servername = "localhost"; 
+$username = "root"; 
+$password = "password"; 
 
-// создание соединения
 $conn = new mysqli($servername, $username, $password);
-// проверка соединения
+
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error); // сообщение об ошибке
+    die("Connection failed: " . $conn->connect_error); 
 }
 
-$sql = "SELECT VERSION()"; //запрос для выборки версии MySQL
-$result = $conn->query($sql); // результат запроса
+$sql = "SELECT VERSION()"; 
+$result = $conn->query($sql); 
 
 if ($result->num_rows > 0) {
-    // вывести данные из каждого столбца, но поскольку их 1 то и выведется только одна строка
+    
     while($row = $result->fetch_assoc()) {
         print_r($row);
     }
 } else {
     echo "0 results";
 }
-$conn->close(); //закрываем соединение с базой данных
+$conn->close(); 
 ?>
